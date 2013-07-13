62ff20991c3caf5617254ca448d2a10b70fe5e4e
dreilly369 <executivewd@hotmail.com>
2013-07-13 16:25:48 -0700

    Cleaning branch for merging

--- test_sorts.py
+++ test_sorts.py
@@ -41,6 +41,8 @@ def run():
 #This function generates sz random strings of length ln.
 #it doesn't account for Capitolization at this time.
 #used for testing the sorting functions in the MultiSorter class
+
+
 def generate_samples(ln,sz):
 	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 	i = 0
